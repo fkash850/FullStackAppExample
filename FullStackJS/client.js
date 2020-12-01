29b0a423639dbdc3f8a618f52fa52038e97cd52c
Fahim Kashem <56698702+fkash850@users.noreply.github.com>
2020-12-01 01:17:48 -0500

    Add files via upload
    logging date

---
+++ FullStackJS/client.js
@@ -0,0 +1,49 @@
+function logDate()
+{
+    console.log('Got into logDate()');
+
+    const request = new XMLHttpRequest();
+
+    request.open("POST", "http://127.0.0.1:3000/logdate", true);
+    request.onload = function ()
+    {
+        console.log('onload complete');
+        let msg = this.response;
+
+        if (request.status === 200 || request.status === 404)
+        {
+            console.log(`Message: ${msg}`);
+        }
+        else
+        {
+            console.log(`Error occurred: Status: ${request.status}`);
+        }
+    }
+
+    request.send();
+}
+
+function logView()
+{
+    console.log('Got into logView()');
+
+    const request = new XMLHttpRequest();
+
+    request.open("GET", "http://127.0.0.1:3000/dates", true);
+    request.onload = function ()
+    {
+        let msg = this.response;
+
+        if (request.status === 200 || request.status === 404)
+        {
+            console.log(`Message: ${msg}`);
+            document.querySelector('#date').innerHTML = msg;
+        }
+        else
+        {
+            console.log(`Error occurred: Status: ${request.status}`);
+        }
+    }
+
+    request.send();
+}
---
+++ FullStackJS/server.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) =>
+{
+    const {method, url} = req;
+    if (method === 'POST' && url === '/logdate')
+    {
+        try {
+            let date = new Date();
+            console.log(`${method} : ${url}`)
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.write('Logged the date!');
+            res.end();
+
+            fs.appendFileSync('datelog.txt', `${date.toUTCString()}\n`);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    else if (method === 'GET' && url === '/dates')
+    {
+        console.log(`${method} : ${url}`);
+        try {
+            let dateContents = fs.readFileSync('datelog.txt');
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.write(`Dates:\n${dateContents}`);
+            res.end();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    else
+    {
+        console.log(`${method} : ${url}`);
+
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.write('Resource not available');
+        res.end();
+    }
+});
+
+server.listen(port, hostname, () =>
+{
+    console.log(`Server running at http://${hostname}:${port}`);
+});
